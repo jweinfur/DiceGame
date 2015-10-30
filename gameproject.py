12106b47010d08c136331a4788dbe25b1c68dc60
jweinfur <jweinfur@pio.carrollu.edu>
2015-10-30 17:37:12 -0500

    Yay more progress

--- gameproject.py
+++ gameproject.py
@@ -8,6 +8,18 @@ print("""Bonus
 AI player
 persistent high score""")
 
+import random
+import sys
+import time
+
+
+def DiceRoll():
+	DiceRoll = random.randint(0,12)
+	DiceRoll2 = random.randint(0,12)
+	print(DiceRoll, DiceRoll2)
+	if DiceRoll == DiceRoll2:
+		print((DiceRoll) * 2)
+	
 
 print("Would you like to play dice?")
 Dice = input ("Y/N?")
@@ -16,8 +28,31 @@ if Dice == "n":
 elif Dice =="y":
 	print("Let's play!")
 	
-while Dice =="y":
-	print("Roll")
-	if Dice == "n":
-		print("Goodbye!")
+print("Player 1, Roll?")
+Roll = input("Y/N?")
+while  Roll =="y":
+	DiceRoll()
 	break
+if Roll == "n":
+	print("Goodbye!")
+	time.sleep(2)
+	sys.exit(0)
+
+
+print("Player 2, Roll?")
+Roll = input("Y/N?")
+while  Roll =="y":
+	DiceRoll()
+	break
+if Roll == "n":
+	print("Goodbye!")
+	time.sleep(2)
+	sys.exit(0)
+	
+	
+
+
+
+
+
+
