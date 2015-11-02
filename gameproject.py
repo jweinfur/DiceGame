5ba2855e5c2eecc0beffdd9161a866dc68743d37
jweinfur <jweinfur@pio.carrollu.edu>
2015-11-02 11:16:00 -0600

    added third die

--- gameproject.py
+++ gameproject.py
@@ -2,22 +2,19 @@ import random
 import time
 import sys
 
-Dice = random.randint(1, 12)
-Dice2 = random.randint(1, 6)
-Dice3 = random.randint(1, 6)
-Dice4 = random.randint(1, 12)
-DoubleDice = ((Dice + Dice2)*2)
-DoubleDice2 = ((Dice3 + Dice4)*2)
-Score1 = (Dice + Dice2)
-Score2 = (Dice3 + Dice4)
-def DiceRoll(Dice, Dice2, Dice3, Dice4):
-	Dice = random.randint(1, 12)
-	Dice2 = random.randint(1, 6)
-	Dice3 = random.randint(1, 12)
-	Dice4 = random.randint(1, 6)
-	DoubleDice = ((Dice + Dice2)*2)
-	DoubleDice2 = ((Dice3 + Dice4)*2)
-	Score1 = (Dice + Dice2)
+
+def DiceRoll():
+	Dice1 = random.randint(1, 20)
+	Dice2 = random.randint(1, 12)
+	Dice3 = random.randint(1,6)
+	Dice4 = random.randint(1, 20)
+	Dice5 = random.randint(1, 12)
+	Dice6 = random.randint (1, 6)
+	DoubleDice1 = (((Dice1 + Dice2)*2) + Dice3)
+	DoubleDice2 = (((Dice4 + Dice5)*2) + Dice6)
+	TripleDice1 = ((Dice1 + Dice2 +Dice3) * 3)
+	TripleDice2 = ((Dice4 + Dice5 +Dice6) * 3)
+	Score1 = (Dice1 + Dice2)
 	Score2 = (Dice3 + Dice4)
 	print("Player 1, Roll?")
 	Roll = input("Y/N?")
@@ -27,13 +24,15 @@ def DiceRoll(Dice, Dice2, Dice3, Dice4):
 		print("Goodbye!")
 		time.sleep(2)
 		sys.exit(0)
-	print(Dice, Dice2)
-	Score1 = (Dice + Dice2)
+	print(Dice1, Dice2, Dice3)
+	Score1 = (Dice1 + Dice2 + Dice3)
 	print(Score1)
-	if Dice == Dice2:
+	if Dice1 == Dice2:
 		Score1 = DoubleDice
-		DoubleDice = ((Dice + Dice2)*2)
-		print(DoubleDice)
+		print(Score1)
+	if Dice1 == Dice2 ==Dice3:
+		Score1 = TripleDice1
+		print(Score1)
 	
 	
 	print("Player 2, Roll?")
@@ -44,13 +43,16 @@ def DiceRoll(Dice, Dice2, Dice3, Dice4):
 		print("Goodbye!")
 		time.sleep(2)
 		sys.exit(0)
-	print(Dice3, Dice4)
-	Score2 = (Dice3 + Dice4)
+	print(Dice4, Dice5, Dice6)
+	Score2 = (Dice4 + Dice5 + Dice6)
 	print(Score2)
-	if Dice3 == Dice4:
+	if Dice4 == Dice5:
 		Score2 = DoubleDice2
-		DoubleDice2 = ((Dice3 + Dice4)*2)
-		print(DoubleDice2)
+		print(Score2)
+	if Dice4 == Dice5 ==Dice6:
+		Score2 = TripleDice2
+		print(Score2)
+	
 	if Score1 > Score2:
 		print("Player 1:", Score1, "Player 2:", Score2)
 		print("Player 1 Wins!")
@@ -77,4 +79,4 @@ if Dice == "n":
 elif Dice =="y":
 	print("Let's play!")
 	
-DiceRoll(Dice, Dice2, Dice3, Dice4)
+DiceRoll()
